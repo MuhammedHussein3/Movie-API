a6df39dc0764eef3e03de94b5ae714e4515fab7c
Muhammad Hussein <156622887+MuhammedHussein3@users.noreply.github.com>
2024-04-15 07:05:06 +0200

    Update MovieController.java

--- MovieApi/src/main/java/com/movieflix/controller/MovieController.java
+++ MovieApi/src/main/java/com/movieflix/controller/MovieController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/movie/")
@@ -29,16 +30,37 @@ public class MovieController {
         return ResponseEntity.status(HttpStatus.CREATED).body(response.toString());
     }
 
-    private MovieDto mapToMovieDto(String movieDto) throws JsonProcessingException {
-
-        ObjectMapper mapper = new ObjectMapper();
-        MovieDto converted = mapper.readValue(movieDto,MovieDto.class);
-        return converted;
-    }
 
     @GetMapping("/id/{id}")
     public ResponseEntity<String> getMovieById(@PathVariable("id") Integer id) throws MovieNotFoundException {
         MovieDto response = movieService.getMovie(id);
         return ResponseEntity.ok(response.toString());
+    }
+
+    @GetMapping("get-movies/")
+    public ResponseEntity<List<MovieDto>> getMovies() {
+        return ResponseEntity.ok(movieService.getAllMovies());
+    }
+
+    @PutMapping("update/{movieId}")
+    public ResponseEntity<MovieDto> updateMovie(@PathVariable Integer movieId,
+                                                @RequestPart MultipartFile file,
+                                                @RequestPart String stringMovieDto) throws IOException, MovieNotFoundException {
+       if (file.isEmpty()) file = null;
+
+       MovieDto response = movieService.updateMovie(movieId,mapToMovieDto(stringMovieDto),file);
+       return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("delete/{movieId}")
+    public ResponseEntity<String> deleteMovie(@PathVariable Integer movieId) throws MovieNotFoundException, IOException {
+        return ResponseEntity.ok(movieService.deleteMovie(movieId));
+    }
+
+    private MovieDto mapToMovieDto(String movieDto) throws JsonProcessingException {
+
+        ObjectMapper mapper = new ObjectMapper();
+        MovieDto converted = mapper.readValue(movieDto,MovieDto.class);
+        return converted;
     }
 }
