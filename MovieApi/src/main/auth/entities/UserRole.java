c9691bf43287364d00f67ef837e2740a3e59844e
Muhammad Hussein <156622887+MuhammedHussein3@users.noreply.github.com>
2024-04-17 17:36:45 +0200

    Delete MovieApi/src/main/auth directory

--- MovieApi/src/main/auth/entities/UserRole.java
+++
@@ -1,6 +0,0 @@
-package com.movieflix.auth.entities;
-
-public enum UserRole {
-    USER,
-    ADMIN
-}
--- MovieApi/src/main/auth/utils/ChangePassword.java
+++
@@ -1,6 +0,0 @@
-package com.movieflix.auth.utils;
-
-
-public record ChangePassword (String password , String repeatPassword) {
-
-}
